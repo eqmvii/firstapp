2eaeb9a2df52bc2d14cf3c0fabce394f04ed05e8
EQMVII <coelomate@gmail.com>
2016-12-01 15:05:00 -0500

    re-written with angular.js

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -1,74 +1,30 @@
 'use strict';
 
-/*NB: Everything is wrapped in an 
-immediately invoked function expression (IIFE)
-to prevent conflicts with other client-side JavaScript being used
-*/
-
-//Find DOM elements in HTML and stores for use
 (function () {
-   var addButton = document.querySelector('.btn-add');
-   var deleteButton = document.querySelector('.btn-delete');
-   var clickNbr = document.querySelector('#click-nbr');
-   var apiUrl = 'http://localhost:3000/api/clicks';
-   
+   angular
+      .module('clementineApp', ['ngResource'])
+      .controller('clickController', ['$scope', '$resource', function ($scope, $resource) {
+         var Click = $resource('/api/clicks');
 
-// Function to fire a specific function once the DOM is loaded 
- function ready (fn) {
-      if (typeof fn !== 'function') {
-         return;
-      }
-
-      if (document.readyState === 'complete') {
-         return fn();
-      }
-
-      document.addEventListener('DOMContentLoaded', fn, false);
-   } 
-
-
-//basic AJAX request function: use GET/POST/Whatever, 
-// when JSON response comes, use CB function with that data
-   function ajaxRequest (method, url, callback) {
-      var xmlhttp = new XMLHttpRequest();
-
-      xmlhttp.onreadystatechange = function () {
-         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-            callback(xmlhttp.response);
+         $scope.getClicks = function () {
+            Click.get(function (results) {
+               $scope.clicks = results.clicks;
+            })
          }
-      };
 
-      xmlhttp.open(method, url, true);
-      xmlhttp.send();
-   }
+         $scope.getClicks();
 
-// Display the click count using received data 
-  function updateClickCount (data) {
-      var clicksObject = JSON.parse(data);
-      clickNbr.innerHTML = clicksObject.clicks;
-   }
+         $scope.addClick = function () {
+            Click.save(function () {
+               $scope.getClicks();
+            });
+         };
 
-//When the page loads, update #click-nbr to current clicks
-ready(ajaxRequest('GET', apiUrl, updateClickCount));
-
-//Makes a POST request to the API, which increments click count
-addButton.addEventListener('click', function () {
-
-      ajaxRequest('POST', apiUrl, function () {
-         ajaxRequest('GET', apiUrl, updateClickCount)
-      });
-
-   }, false); 
-
-//makes a DELETE post request to the API, which resets click count
-deleteButton.addEventListener('click', function () {
-
-      ajaxRequest('DELETE', apiUrl, function () {
-         ajaxRequest('GET', apiUrl, updateClickCount);
-      });
-
-   }, false); 
-
+         $scope.resetClicks = function () {
+            Click.remove(function () {
+               $scope.getClicks();
+            })
+         };
+      }]);
 })();
-
 
